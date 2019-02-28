c27d2b0ffa4a889e2b3b48cadfc140e7200704b6
RoySan <maxthompson141@hotmail.co.uk>
2019-02-28 17:46:59 +0000

    for luke to look at

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,7 +14,7 @@ class BookingsController < ApplicationController
 
   # GET /bookings/new
   def new
-    @booking = Booking.new
+    @bookings = Booking.all
   end
 
   # GET /bookings/1/edit
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,4 @@
 class Booking < ApplicationRecord
+
+
 end
