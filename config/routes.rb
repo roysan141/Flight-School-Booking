84a66809db8608c99aca73dc6afeadd5ebcfb12a
Luke Roberts <email@luke-roberts.co.uk>
2019-03-06 13:06:02 +0000

    some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  root to: 'pages#home'
-  resources :bookings
-  resources :schedules
   devise_for :users
-  root :to => 'schedules#index', :constraints => lambda { |request| request.env['warden'].user.is_trainer?}
-root :to => 'bookings#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ActiveAdmin.routes(self)
+
+  resources :bookings
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'pages#home'
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new'
+    end
+    root :to => 'pages#home'
+  end
 end
