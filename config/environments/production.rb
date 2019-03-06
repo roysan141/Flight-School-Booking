f94d607cc655c41297779f60d07427426efed954
RoySan <maxthompson141@hotmail.co.uk>
2019-03-06 18:22:13 +0000

    add url base options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Flight-School-Booking_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "taranisbooking.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :authentication => :plain,
