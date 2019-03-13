0f387548159fdca9d1b1107bd5bf703a67fdc4eb
RoySan <maxthompson141@hotmail.co.uk>
2019-03-13 15:47:09 +0000

    updated as per comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to edit_user_path, notice: 'Profile was successfully updated.' }
+        format.html { redirect_to edit_user_path(@user), notice: 'Profile was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
